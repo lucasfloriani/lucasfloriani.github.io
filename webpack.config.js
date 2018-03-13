a11f214d91d1a53dc02d194cfc39f9e0aab38c93
Lucas Floriani <lucas.floriane@gmail.com>
2018-03-12 23:16:25 -0300

    More sass configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -19,9 +21,40 @@ module.exports = {
         }
       },
       {
-        test: /\.sass$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
-      },
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              minimize: {
+                safe: true
+              }
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ["last 2 versions"]
+              },
+              plugins: () => [
+                autoprefixer
+              ]
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {}
+          }
+        ]
+      }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+      chunkFilename: "style.css"
+    })
+  ],
 }
